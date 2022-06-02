91b52b41c6180cff4929c9479f0c94f5810fd0fd
Doruk Bulut <76884139+dorukbulut@users.noreply.github.com>
2022-06-02 22:38:09 +0300

    Add all files

---
+++ script.js
@@ -0,0 +1,10 @@
+let h3 = document.querySelector("h3");
+let color1 = document.querySelector(".color1");
+let color2 = document.querySelector(".color2");
+let body = document.getElementById("gradient");
+
+let setGradient = ()=>{
+    body.style.background = "linear-gradient(to right," + color1.value + "," + color2.value + ")";
+    h3.textContent = body.style.background + ";" ; 
+}
+
